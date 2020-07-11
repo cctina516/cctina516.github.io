204c32292e290485efe42dcf276b6df2ddb1bed5
cctina516 <christinayan1019@gmail.com>
2020-07-11 15:53:28 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,345 @@
 import React from 'react';
 import logo from './logo.svg';
+//import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
+import Portfolio from './components/Portfolio'
 import './App.css';
 
 function App() {
+  const portfolioLinks = [
+    {
+      title: 'Title',
+      caption: 'text'
+    },
+    {
+      title: 'Title',
+      caption: 'text'
+    },
+    {
+      title: 'Title',
+      caption: 'text'
+    }
+
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div className="container">
+      {/* <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a> */}
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav text-uppercase ml-auto">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">Education</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#portfolio">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#about">Experience</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#team">Skills</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        {/* <div className="intro-lead-in">Welcome To Our Studio!</div> */}
+        <div className="intro-heading text-uppercase">Christina Yan</div>
+        
+        <ul className="list-inline social-buttons">
+          <li className="list-inline-item">
+            <a href="https://www.linkedin.com/in/chutingyan/">
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://github.com/cctina516">
+              <i className="fa fa-github"></i>
+            </a>
+          </li>
+        </ul>
+        {/* <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a> */}
+      </div>
+    </div>
+  </header>
+
+  
+  <section className="page-section" id="services">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Education</h2>
+          {/* <h3 className="section-subheading text-muted">text goes in here.</h3> */}
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">CS</h4>
+          <p className="text-muted">text goes in here.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-university fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">MBA</h4>
+          <p className="text-muted">text.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-desktop fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">CS</h4>
+          <p className="text-muted">text text text.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+
+  
+  <section className="page-section" id="about">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Experience</h2>
+          {/* <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <ul className="timeline">
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/1.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>Time </h4>
+                  <h4 className="subheading">Text</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>Time</h4>
+                  <h4 className="subheading">Text</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>December 2012</h4>
+                  <h4 className="subheading">Text</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>July 2014</h4>
+                  <h4 className="subheading">Text</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <h4>To Be
+                  <br/>Continued!
+                  </h4>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="bg-light page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Skills</h2>
+          <h3 className="section-subheading text-muted">PROGRAMMING LANGUAGES & TOOLS</h3>
+        </div>
+      </div>
+      {/* <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
+            <h4>Programming Languages</h4>
+            <p className="text-muted">Java, JavaScript, Python, HTML, CSS, C++</p>
+
+
+          </div>
+        </div>
+ 
+      </div> */}
+      <div className="row">
+        <div className="col-lg-8 mx-auto text-center">
+          <h4>Programming Languages</h4>
+            <p className="text-muted">Java, JavaScript, Python, HTML, CSS, C++</p>
+          <h4>Frameworks and tools</h4>
+            <p className="text-muted">React, React Native, Git, MongoDB, AWS, Firebase, Android Studio, Xcode</p>
+          {/* <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p> */}
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="py-5">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="page-section" id="contact">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Keep In Touch</h2>
+          {/* <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; Your Website 2020</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+            <li className="list-inline-item">
+              <a href="https://www.linkedin.com/in/chutingyan/">
+                <i className="fa fa-linkedin"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="https://github.com/cctina516">
+                <i className="fa fa-github"></i>
+              </a>
+            </li>
+          </ul>
+  
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
     </div>
   );
 }
